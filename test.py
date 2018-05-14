105d9957a9c2b8a769429757a90051b53a0be8a8
Michelle Fernandez Bieber <mfbieber@gmail.com>
2018-05-14 16:16:37 +0200

    changed Haffson pw, put into a file (will not be on github - of course), made test.py read the file containing the pw...

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+file = open("haffsonpw", "r")
+pw = file.read()
 from pyArango.connection import *
-conn = Connection(arangoURL = "https://arango.research.haffson.org", username="haffson", password="")
+conn = Connection(arangoURL = "https://arango.research.haffson.org", username="haffson", password=pw)
 print(conn.databases)
